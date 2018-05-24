012890fe6c7c826ed1ab780e43e467bca44619af
朱远宏 <@MSI>
2018-05-24 20:46:29 +0800

    Signed-off-by: 朱远宏 <朱远宏@MSI> hkj

---
+++ Test1/module1/test.py
@@ -0,0 +1,12 @@
+'''
+Created on 2018年5月22日
+
+@author: 朱远宏
+'''
+
+import sys
+print('================Python import mode==========================');
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为',sys.path)
